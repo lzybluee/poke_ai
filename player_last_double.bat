757ab0c48a31300da3c59c390b3d27525486a716
lzybluee <lzybluee@gmail.com>
2024-03-25 09:23:10 +0800

    Don't keep set env.

--- player_last_double.bat
+++ player_last_double.bat
@@ -1,2 +1,1 @@
-set PLAYER_CONTROL_AI=True
-node ..\pokemon-showdown\dist\sim\examples\battle-ai.js gen7doublescustomgame Team_Player.txt Team_AI.txt
+cmd /v /c "set PLAYER_CONTROL_AI=True && node ..\pokemon-showdown\dist\sim\examples\battle-ai.js gen7doublescustomgame Team_Player.txt Team_AI.txt"
