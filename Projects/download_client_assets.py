62879c7cc51c5f79af2957b35ce338b082bf74ed
lzybluee <lzybluee@gmail.com>
2025-10-11 10:23:46 +0800

    Add script to download client assets

--- Projects/download_client_assets.py
+++ Projects/download_client_assets.py
@@ -2,6 +2,7 @@ import os
 import re
 import time
 from concurrent.futures import ThreadPoolExecutor
+from urllib.parse import unquote
 
 import requests
 
@@ -14,6 +15,10 @@ folders = [
     'sprites/afd-back',
     'sprites/afd-back-shiny',
     'sprites/afd-shiny',
+    'sprites/ani',
+    'sprites/ani-back',
+    'sprites/ani-back-shiny',
+    'sprites/ani-shiny',
     'sprites/categories',
     'sprites/dex',
     'sprites/dex-shiny',
@@ -33,10 +38,17 @@ LOCAL_FOLDER = r'E:\Poke\assets'
 
 
 def download_asset(url, path):
+    if os.path.exists(path):
+        return
+
     retry = True
     while retry:
         try:
             response = requests.get(url, timeout=20)
+            if response.status_code != 200:
+                if response.status_code != 404:
+                    print(f'{url} => {path} Error Code:', response.status_code)
+                return
             with open(path, 'wb') as file:
                 file.write(response.content)
             retry = False
@@ -54,7 +66,8 @@ def download_url(folder):
     result = re.findall(r'<a class="row" href="\./(.*?)">', html)
     for asset in result:
         if '.' in asset and asset[asset.rindex('.'):] in suffix:
-            download_asset(f'{url}/{asset}', os.path.join(LOCAL_FOLDER, folder, asset))
+            path = os.path.join(LOCAL_FOLDER, *folder.split('/'), unquote(asset).replace('?', '-'))
+            download_asset(f'{url}/{asset}', path)
 
 
 def main():
